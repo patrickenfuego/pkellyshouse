6383b25e46c53f7d86bab0f56c66b7f421c79e18
patrickenfuego <patrick.m.kelly1989@gmail.com>
2020-04-26 17:41:53 -0400

    Basic About page layout and Intro Tab component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { Tabs, Tab } from "react-bootstrap";
 
 // eslint-disable-next-line no-unused-vars
-import Header from "./Header";
+import Intro from "./Intro";
 
 const About = () => {
   return (
-    <div className="about-area">
-      <h1>Some text here</h1>
-      <p>more stuff...</p>
-    </div>
+    <body className="about-area">
+      <Tabs defaultActiveKey="intro" id="uncontrolled-tab-example">
+        <Tab eventKey="intro" title="About Me">
+          <Intro />
+        </Tab>
+        <Tab eventKey="resume" title="Resume">
+          <p>Some more stuff</p>
+        </Tab>
+        <Tab eventKey="contact" title="Contact">
+          <p>Even more stuff</p>
+        </Tab>
+      </Tabs>
+    </body>
   );
 };
 
---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Intro = () => {
+  return (
+    <div className="tab-container">
+      <div className="inner-container">
+        <h1>header in intro</h1>
+        <p>some text in intro area</p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
