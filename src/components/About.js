0c63560cf94e8fdec580903625e0da13a57c6a49
Patrick Kelly <pkelly@oakland.edu>
2020-05-03 15:33:13 -0400

    Setup card layout on Intro tab

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,57 @@ import { Tabs, Tab } from "react-bootstrap";
 // eslint-disable-next-line no-unused-vars
 import Intro from "./Intro";
 
+const cardArray = [
+  {
+    img: "/assets/images/logo512.png",
+    alt: "Some alt text",
+    title: "Card Title",
+    body:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    img: "/assets/images/powershell.png",
+    alt: "Some alt text",
+    title: "Card Title",
+    body:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    img: "/assets/images/azureAD.png",
+    alt: "Some alt text",
+    title: "Card Title",
+    body:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    img: "/assets/images/spring_logo.png",
+    alt: "Some alt text",
+    title: "Card Title",
+    body:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    img: "/assets/images/pk_logo.png",
+    alt: "Some alt text",
+    title: "Card Title",
+    body:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    img: "/assets/images/pk_logo.png",
+    alt: "Some alt text",
+    title: "Card Title",
+    body:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }
+];
+
 const About = () => {
   return (
-    <body className="about-area">
+    <div className="about-area">
       <Tabs defaultActiveKey="intro" id="uncontrolled-tab-example">
         <Tab eventKey="intro" title="About Me">
-          <Intro />
+          <Intro cards={cardArray} />
         </Tab>
         <Tab eventKey="resume" title="Resume">
           <p>Some more stuff</p>
@@ -18,7 +63,7 @@ const About = () => {
           <p>Even more stuff</p>
         </Tab>
       </Tabs>
-    </body>
+    </div>
   );
 };
 
